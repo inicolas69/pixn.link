a3cf3667316fbcca11b2281e8adb8c05f70b4107
Nicolas Iniesta <inicolas69@gmail.com>
2020-08-04 00:25:24 +0200

    First commit

---
+++ server/index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const yup = require('yup');
+const monk = require('monk');
+const { nanoid } = require('nanoid');
+
+require('dotenv').config();
+
+const db = monk(process.env.MONGO_URI);
+const urls = db.get('urls');
+urls.createIndex({ slug: 1 }, { unique: true });
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+app.use(express.static('./public'));
+
+app.get('/:id', async (req, res) => {
+  // Short url redirection
+  const { id: slug } = req.params;
+  try {
+    const url = await urls.findOne({ slug });
+    if (url) {
+      res.redirect(url.url);
+    }
+    res.redirect(`/?error=${slug} not fount`);
+  } catch (error) {
+    res.redirect(`/?error=Link not fount`);
+  }
+});
+
+const schema = yup.object().shape({
+  slug: yup
+    .string()
+    .trim()
+    .matches(/[\w\-]/i),
+  url: yup.string().trim().url().required(),
+});
+
+app.post('/url', async (req, res, next) => {
+  // Short url creation
+  let { slug, url } = req.body;
+  try {
+    await schema.validate({
+      slug,
+      url,
+    });
+    if (!slug) {
+      slug = nanoid(5);
+    } else {
+      const existing = await urls.findOne({ slug });
+      if (existing) {
+        throw new Error('Raccourci déjà pris.');
+      }
+    }
+    slug = slug.toLowerCase();
+    const newUrl = {
+      url,
+      slug,
+    };
+    const created = await urls.insert(newUrl);
+    res.json(created);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((error, req, res, next) => {
+  if (error.status) {
+    res.status(error.status);
+  }
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? '😃' : error.stack,
+  });
+});
+
+const port = process.env.PORT || 1234;
+
+app.listen(port, () => {
+  console.log(`Listening http://localhost:${port}`);
+});
