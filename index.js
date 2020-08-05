f0ff8ebc2d7053782ab98e6b62aae74c68de0986
Nicolas Iniesta <inicolas69@gmail.com>
2020-08-05 16:05:53 +0200

    Modif titre page index

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const yup = require('yup');
@@ -16,7 +15,6 @@ const app = express();
 
 app.use(helmet());
 app.use(morgan('tiny'));
-app.use(cors());
 app.use(express.json());
 app.use(express.static('./public'));
 
